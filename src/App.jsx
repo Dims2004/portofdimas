809f82494a9cdc64d4bc3b0ccd2760f503374e7a
Dims2004 <162020815+Dims2004@users.noreply.github.com>
2026-09-17 18:57:25 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import Works from "./pages/Works";
 import WorkDetail from "./pages/WorkDetail";
 import Story from "./pages/Story";
+import Certifications from "./pages/Certifications";
 import NotFound from "./pages/NotFound";
 
 function PageTransition({ children }) {
@@ -38,6 +39,7 @@ export default function App() {
             <Route path="/karya" element={<Works />} />
             <Route path="/karya/:slug" element={<WorkDetail />} />
             <Route path="/cerita" element={<Story />} />
+            <Route path="/sertifikasi" element={<Certifications />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </PageTransition>
